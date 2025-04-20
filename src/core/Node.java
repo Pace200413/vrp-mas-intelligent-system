3f4de45185e850978d9b33a195b21f70a0ff459b
Islam Mamedov <dexterjamescole@gmail.com>
2025-04-20 21:06:11 +0800

    Update Node.java

--- src/core/Node.java
+++ src/core/Node.java
@@ -1,1 +1,19 @@
+package core;
+
+public class Node {
+    public int ID, x, y, demand;
+
+    public Node(int ID, int x, int y, int demand) {
+        this.ID = ID;
+        this.x = x;
+        this.y = y;
+        this.demand = demand;
+    }
+
+    public double distanceTo(Node other) {
+        int dx = this.x - other.x;
+        int dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
 
