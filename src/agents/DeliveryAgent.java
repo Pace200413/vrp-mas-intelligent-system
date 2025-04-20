fa653fcb9006b51f5d53dc8e143357ee0c217b4a
Islam Mamedov <dexterjamescole@gmail.com>
2025-04-20 21:02:28 +0800

    Update DeliveryAgent.java

--- src/agents/DeliveryAgent.java
+++ src/agents/DeliveryAgent.java
@@ -1,1 +1,23 @@
+package agents;
+
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+
+public class DeliveryAgent extends Agent {
+
+    protected void setup() {
+        System.out.println("[INFO] " + getLocalName() + " started and waiting for route...");
+
+        addBehaviour(new jade.core.behaviours.CyclicBehaviour() {
+            public void action() {
+                ACLMessage msg = receive();
+                if (msg != null) {
+                    System.out.println("[" + getLocalName() + "] received: " + msg.getContent());
+                } else {
+                    block(); // Wait for message
+                }
+            }
+        });
+    }
+}
 
