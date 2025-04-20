7162fc222c1f67022ba9f4e69e841ab9bb5021a7
Islam Mamedov <dexterjamescole@gmail.com>
2025-04-20 21:06:00 +0800

    Update NearestNeighborSolver.java

--- src/core/NearestNeighborSolver.java
+++ src/core/NearestNeighborSolver.java
@@ -1,1 +1,54 @@
+package core;
+
+import java.util.*;
+
+public class NearestNeighborSolver {
+    public ArrayList<Route> generateRoutes(VRPInstance instance) {
+        ArrayList<Route> routes = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        while (visited.size() < instance.customers.size()) {
+            Route route = new Route();
+            Node current = instance.depot;
+
+            while (true) {
+                Node next = findNearestFeasible(current, instance.customers, visited, route);
+                if (next == null) break;
+                route.addCustomer(next);
+                visited.add(next.ID);
+                current = next;
+            }
+
+            routes.add(route);
+        }
+
+        return routes;
+    }
+
+    private Node findNearestFeasible(Node current, List<Node> customers, Set<Integer> visited, Route route) {
+        Node nearest = null;
+        double minDist = Double.MAX_VALUE;
+
+        for (Node c : customers) {
+            if (visited.contains(c.ID)) continue;
+            if (!route.canAdd(c)) continue;
+
+            double dist = current.distanceTo(c);
+            if (dist < minDist) {
+                minDist = dist;
+                nearest = c;
+            }
+        }
+
+        return nearest;
+    }
+
+    public void printRoutes(ArrayList<Route> routes, Node depot) {
+        int i = 1;
+        for (Route route : routes) {
+            System.out.println("Route " + i++ + ": " + route);
+            System.out.printf("  Load: %d / %d | Distance: %.2f\n", route.load, route.capacity, route.calculateTotalDistance(depot));
+        }
+    }
+}
 
